786b14efa66157a161ad58d7a6063a91914f5472
Mikhail Gordeev <miha_gord@mail.ru>
2025-09-17 13:11:13 +0700

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,12 +1,14 @@
 #11
 s = input().split()
+
 for i in range(1, len(s)):
     if s[i-1][-1] != s[i][0]:
         if i % 2 == 0:
-            print('Вася')
+            print('Вася') 
         else:
             print('Петя')
         break
+        
 else:
     if len(s) % 2 == 0:
         print('Вася')
